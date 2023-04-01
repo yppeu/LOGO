0c82c51147dd76dbcf2532ee8158ed18b2a0080c
sua yeon <yppeu@naver.com>
2023-04-01 12:14:38 +0900

    0401

--- js/index.js
+++ js/index.js
@@ -6,4 +6,20 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    // 화면의 넓이가 320px 이상일 때
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // 화면의 넓이가 640px 이상일 때
+    670: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
 });
